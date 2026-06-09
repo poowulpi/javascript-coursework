1854c05128efe880baca260f6292a08d434f07b7
poowulpi <artbykjean@gmail.com>
2026-06-09 14:44:24 -0400

    Test

---
+++ assignment-1/dataTypes.js
@@ -0,0 +1,13 @@
+// Primitive data types in JavaScript
+
+let age = 31;
+let fullName = "Katie Jean";
+let isEnrolled = true;
+let futureGoal;
+let completedCourse = null;
+
+console.log("Age:", age, "| Type:", typeof age);
+console.log("Name:", fullName, "| Type:", typeof fullName);
+console.log("Enrolled:", isEnrolled, "|Type:", typeof isEnrolled);
+console.log("Future Goals:", futureGoal, "| Type:", typeof futureGoal);
+console.log("Completed Course:", completedCourse, "| Type:", typeof completedCourse);
