28575f28ded3b83943fe7a7786b1673661d0acdc
poowulpi <artbykjean@gmail.com>
2026-06-09 14:49:38 -0400

    Actually committing and not just testing VS Code lol

--- assignment-1/dataTypes.js
+++ assignment-1/dataTypes.js
@@ -11,3 +11,9 @@ console.log("Name:", fullName, "| Type:", typeof fullName);
 console.log("Enrolled:", isEnrolled, "|Type:", typeof isEnrolled);
 console.log("Future Goals:", futureGoal, "| Type:", typeof futureGoal);
 console.log("Completed Course:", completedCourse, "| Type:", typeof completedCourse);
+
+/*
+I learned that undefined means a variable has been declared but a value has not been given yet.
+Null is used when I WANT a variable to have no value.
+Undefined happens automatically, while null is a value that I assign myself.
+*/
