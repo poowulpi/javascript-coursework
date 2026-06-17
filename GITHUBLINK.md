2539fd059bb0a61f054f55f92ee4ce930739fa80
poowulpi <artbykjean@gmail.com>
2026-06-17 13:21:33 -0400

    Assignment 2 - Begin

---
+++ GITHUBLINK.md
@@ -0,0 +1,1 @@
+https://github.com/poowulpi/javascript-coursework
---
+++ assignment-2/assignment-2.js
@@ -0,0 +1,10 @@
+// Add up all the numbers in an array and print the total
+
+let numbers = [5, 10, 15, 20];
+let total = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+}
+
+console.log("Total:", total);
