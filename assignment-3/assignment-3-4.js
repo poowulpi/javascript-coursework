5acf89119a7a9f73f3b80b76dc40da4d83db7752
poowulpi <artbykjean@gmail.com>
2026-07-01 10:33:36 -0400

    problem 1

---
+++ assignment-3/assignment-3-4.js
@@ -0,0 +1,14 @@
+//////////////* Problem 1: Is It Positive, Negative, or Zero? */
+
+let num = 5; // Test with different values: 5, -3, 0
+
+if (num > 0) {
+    console.log("positive");
+} else if (num < 0) {
+    console.log("negative");
+} else {
+    console.log("zero");
+}
+
+/* POSITIVE! */
+
