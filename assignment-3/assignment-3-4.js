08d5ede46760036277ac0dc2c3e75d4195c73b6a
poowulpi <artbykjean@gmail.com>
2026-07-01 10:35:13 -0400

    problem 2

--- assignment-3/assignment-3-4.js
+++ assignment-3/assignment-3-4.js
@@ -1,4 +1,4 @@
-//////////////* Problem 1: Is It Positive, Negative, or Zero? */
+/* Problem 1: Is It Positive, Negative, or Zero? */
 
 let num = 5; // Test with different values: 5, -3, 0
 
@@ -12,3 +12,17 @@ if (num > 0) {
 
 /* POSITIVE! */
 
+/* Problem 2: Add Only Positive Numbers */
+
+let numbers = [1, -2, 3, -4, 5];
+let total = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        total += numbers[i];
+    }
+}
+
+console.log("Total of positive numbers: " + total);
+
+/* Total of positive numbers: 9 */
